e405f6c1efdd1c85ae4f1acd0a4897f85764d6d4
hot-handsomechicken <hot-handsomechicken@users.noreply.github.com>
2026-08-14 15:39:41 +0800

    Publish game on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import { sites } from "./build/sites-vite-plugin.js";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/penny-game/" : "/",
   plugins: [sites(), cloudflare()],
 });
